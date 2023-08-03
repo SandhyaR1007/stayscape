b770b1dc22c97aa4069447f8d2759d2f65e9af0c
SandhyaR1007 <grajwanshi1008@gmail.com>
2023-08-04 01:04:50 +0530

    update: index

---
+++ src/components/index.tsx
@@ -0,0 +1,3 @@
+export { default as Header } from "./header/Header";
+export { default as Categories } from "./header/Categories";
+export { default as PropertyListing } from "./properties/PropertyListing";
