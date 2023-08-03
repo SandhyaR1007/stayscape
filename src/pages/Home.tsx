e23a97afe83f86abeecb5252cabc6fbfeb052d23
SandhyaR1007 <grajwanshi1008@gmail.com>
2023-08-04 00:45:35 +0530

    add: Home with Categories

---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Categories } from "../components/header";
+
+const Home = () => {
+  return (
+    <div>
+      <Categories />
+    </div>
+  );
+};
+
+export default Home;
