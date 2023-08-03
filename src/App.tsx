1204709b24cb8d77d347e9369d149b7096e7397d
SandhyaR1007 <grajwanshi1008@gmail.com>
2023-08-04 00:46:09 +0530

    update: added Header and Home

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,11 @@
+import { Header } from "./components/header";
+import { Home } from "./pages";
+
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+      <Header />
+      <Home />
+    </>
+  );
 }
